4aeb0ec9636e0c390e72f476b74f450ea94648e5
Alex <sasha.man@mail.ru>
2020-07-22 15:19:17 +0300

    helloooo

---
+++ Assets/Scripts/Int.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Int : MonoBehaviour
+
+{
+        static double LeftTriangle(Func<double, double> f, double a, double b, int n)
+        {
+            var h = (b - a) / n;
+            var sum = 0d;
+            for (var i = 0; i <= n - 1; i++)
+            {
+                var x = a + i * h;
+                sum += f(x);
+            }
+
+            var result = h * sum;
+            return result;
+        }
+
+        static double RightTriangle(Func<double, double> f, double a, double b, int n)
+        {
+            var h = (b - a) / n;
+            var sum = 0d;
+            for (var i = 1; i <= n; i++)
+            {
+                var x = a + i * h;
+                sum += f(x);
+            }
+
+            var result = h * sum;
+            return result;
+        }
+
+        static double CentralTriangle(Func<double, double> f, double a, double b, int n)
+        {
+            var h = (b - a) / n;
+            var sum = 0d;
+            for (var i = 0; i < n; i++)
+            {
+                var x = a + i / 2d * h;
+                sum += f(x);
+            }
+
+            var result = h * sum;
+            return result;
+        }
+
+        static void Calc(string[] args)
+        {
+            //локальная функция
+            
+
+           
+
+          
+        }
+    
+        void Start()
+{
+        double f(double x) => x*x ;
+        double result = LeftTriangle(f, 5, 6, 1000);
+        Debug.Log("Формула левых прямоугольников: " + result);
+    }
+}
---
+++ Assets/Scripts/TestMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TestMove : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] float velocity;
+
+    private Rigidbody playerRb;
+    private GameObject centerOfMass;
+    public float speed = 5f;
+    public float turnSpeed = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        centerOfMass = GameObject.Find("Center Of Mass");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(centerOfMass.transform.forward * speed * forwardInput);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * turnSpeed);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(- Vector3.forward * Time.deltaTime * turnSpeed);
+        }
+          if (Input.GetKey(KeyCode.Z))
+        {
+            playerRb.AddForce(centerOfMass.transform.up * speed);
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            playerRb.AddForce(- centerOfMass.transform.up * speed);
+        }
+
+        // spedometer
+        velocity = Mathf.Round(playerRb.velocity.magnitude * 3.6f);
+        speedometerText.SetText("Velocity: " + velocity + "kph");
+
+    }
+}
