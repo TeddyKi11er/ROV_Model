f4f81ee0d332d1b89a23e840af59e5e17d4f09bf
Eugene <ewegexa@gmail.com>
2020-08-03 17:59:28 +0300

    добавлены части аппарата

---
+++ Assets/Scripts/Carcass.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Service;
+using System.Dynamic;
+
+namespace ServDynamicsModuleice
+{
+    /// <summary>
+    /// Класс инкапсулирующий все характристики корпуса аппарата
+    /// и методы работы с ними
+    /// </summary>
+    internal class Carcass
+    {
+        private double m_;
+        private double length_;
+        private double width_;
+        private double height_;
+        private double V_;
+
+        // сила архимеда, действующая на па
+        private Vector3 Fa_;
+        // момент силы архимеда
+        private Vector3 P_;
+        private Vector3 sMidelya_;
+        private Vector3 centrVolume_;
+        private Vector3 hydrodynamiСoef_;
+        private Vector3 inercApparat_;
+        // вес подводного аппарата
+        private Vector3 MFa_;
+
+        // этот класс, как и его конструктор существуют только потому, что
+        // мне не равится, что все данные хранятся в перемешку в классе 
+        // Config
+        public Carcass(Config config)
+        {
+            m_ = config.m;
+            length_ = config.length;
+            width_ = config.width;
+            height_ = config.height;
+            V_ = length_ * width_ * height_;
+
+            Fa_ = new Vector3(0, (float)(1000 * 9.81 * V_), 0);
+            P_ = new Vector3(0, (float)(-m_ * 9.81), 0);
+
+            sMidelya_ = new Vector3((float)config.sMidelya.x, 
+                                    (float)config.sMidelya.y,
+                                    (float)config.sMidelya.z);
+            centrVolume_ = new Vector3((float)config.centrVolume.x, 
+                                       (float)config.centrVolume.y,
+                                       (float)config.centrVolume.z);
+            hydrodynamiСoef_ = new Vector3((float)config.hydrodynamiСoef.x, 
+                                           (float)config.hydrodynamiСoef.y,
+                                           (float)config.hydrodynamiСoef.z);
+            inercApparat_ = new Vector3((float)config.inercApparat.x, 
+                                        (float)config.inercApparat.y,
+                                        (float)config.inercApparat.z);
+            MFa_ = Vector3.Cross(Fa_, centrVolume_);
+        }
+
+
+        // свойства класса, для более естественного использования
+        // данных, строго только для чтения
+        public double m
+        {
+            get
+            {
+                return m_;
+            }
+        }
+
+        public double length
+        {
+            get
+            {
+                return length_;
+            }
+        }
+
+        public double width
+        {
+            get
+            {
+                return width_;
+            }
+        }
+
+        public double height
+        {
+            get
+            {
+                return height_;
+            }
+        }
+
+        public double V
+        {
+            get
+            {
+                return V_;
+            }
+        }
+
+        public Vector3 Fa
+        {
+            get
+            {
+                return new Vector3(Fa_.x, Fa_.y, Fa_.z);
+            }
+        }
+
+        public Vector3 P
+        {
+            get
+            {
+                return new Vector3(P_.x, P_.y, P_.z);
+            }
+
+        }
+
+        public Vector3 sMidelya
+        {
+            get
+            {
+                return new Vector3(sMidelya_.x, sMidelya_.y,
+                                   sMidelya_.z);
+            }
+        }
+
+        public Vector3 centrVolume
+        {
+            get
+            {
+                return new Vector3(centrVolume_.x, centrVolume_.y,
+                                   centrVolume_.z);
+            }
+        }
+
+        public Vector3 hydrodynamiСoef
+        {
+            get
+            {
+                return new Vector3(hydrodynamiСoef_.x, hydrodynamiСoef_.y,
+                                   hydrodynamiСoef_.z);
+            }
+        }
+
+        public Vector3 inercApparat
+        {
+            get
+            {
+                return new Vector3(inercApparat_.x, inercApparat_.y,
+                                   inercApparat_.z);
+            }
+        }
+
+        public Vector3 MFa
+        {
+            get
+            {
+                return new Vector3(MFa_.x, MFa_.y, MFa_.z);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WMA.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Service;
+using System.Dynamic;
+
+namespace ServDynamicsModuleice
+{
+    /// <summary>
+    /// Данный класс инкапсулирует данные и методы расчета для
+    /// винтоматорного агрегата
+    /// </summary>
+    internal class WMA
+    {
+        private double Fmax;
+        private Vector3 position;
+        private Vector3 deltaF;
+        private Vector3 F;
+        private Vector3 M;
+
+        public WMA(double Fmax, threeValues position, int number)
+        {
+            if (Fmax <= 0)
+            {
+                throw new Exception("ошибка при инициализации двигателя: " + number +
+                                    " (неправильная максимальная сила)");
+            }
+            this.Fmax = Fmax;
+            this.position = new Vector3((float)position.x, (float)position.y,
+                                        (float)position.z);
+            switch (number)
+            {
+                case 1:
+                case 2:
+                    this.deltaF = Vector3.right;
+                    break;
+                case 3:
+                case 4:
+                    this.deltaF = Vector3.up;
+                    break;
+                default:
+                    throw new Exception("ошибка при инициализации двигателя" +
+                                        " неправильный номер " + number);
+                    break;
+            }
+            this.F = Vector3.zero;
+            this.M = Vector3.zero;
+        }
+
+        public WMA(double Fmax, threeValues position, threeValues direction)
+        {
+            if (Fmax <= 0)
+            {
+                throw new Exception("ошибка при инициализации двигателя" +
+                                    "(неправильная максимальная сила)");
+            }
+            this.Fmax = Fmax;
+            this.position = new Vector3((float)position.x, (float)position.y,
+                                        (float)position.z);
+            this.position = new Vector3((float)direction.x, (float)direction.y,
+                                        (float)direction.z);
+            this.F = Vector3.zero;
+            this.M = Vector3.zero;
+        }
+
+        /// <summary>
+        /// Конструктор копирования, запрещен к использованию 
+        /// </summary>
+        /// <param name="val"></param>
+        private WMA(WMA val) { }
+
+
+        /// <summary>
+        /// Рассчитывает тыгу по сигналу управления, за основу взято 
+        /// апериодическое звено T = 0.15 c
+        /// </summary>
+        /// <param name="signal"></param>
+        public void setValF(double signal)
+        {
+            double lenF = 0;
+
+            // этот if нужен чтобы учесть направление силы тяги
+            if (Vector3.Dot(deltaF, F) > 0)
+            {
+                // реализует апериодическое звено
+                lenF = (signal - F.magnitude / Fmax) / 0.15 * Time.deltaTime;
+            }
+            else
+            {
+                lenF = (signal + F.magnitude / Fmax) / 0.15 * Time.deltaTime;
+            }
+            F = F + (deltaF * (float)lenF);
+            // если на вход подается линейная функция, стоит проверить 
+            // не превысила ли тяга максимальное значение
+            if (F.magnitude > Fmax)
+            {
+                F = F * ((float)Fmax / F.magnitude);
+            }
+            // посчитаем новый момент двигателя относительно цм
+            setM();
+        }
+
+        /// <summary>
+        /// Считает момент двигателя относительно цм, вызывается
+        /// методом setValF, не вижу смысла делать этот метод публичным
+        /// </summary>
+        private void setM()
+        {
+            M = Vector3.Cross(F, position);
+        }
+
+
+        /// <summary>
+        /// Метод для получения значения вектора силы
+        /// </summary>
+        /// <returns></returns>
+        public Vector3 getF()
+        {
+            return new Vector3(F.x, F.y, F.z);
+        }
+
+        /// <summary>
+        /// Метод для получения значения вектора момента
+        /// </summary>
+        /// <returns></returns>
+        public Vector3 getM()
+        {
+            return new Vector3(M.x, M.y, M.z);
+        }
+    }
+}
